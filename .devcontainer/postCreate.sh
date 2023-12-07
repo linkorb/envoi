ec6203294d65413371cc63cfc908739a8cdf2b66
Marius Ghita <ghita.v.marius@gmail.com>
2023-12-07 18:10:37 +0200

    chore: repo-ansible run

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -7,5 +7,6 @@ cp .devcontainer/git/hooks/pre-push .git/hooks/pre-push
 chmod +x .git/hooks/pre-push
 
 composer config --global --auth http-basic.repo.packagist.com "$GITHUB_USER" "$PACKAGIST_TOKEN"
+
 composer install
 
