8a6ac1670c9ccf9268409287ce7cb242542e2c4a
Marius Ghita <ghita.v.marius@gmail.com>
2023-12-07 18:12:55 +0200

    chore: no private packagist auth in devcontainer

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -6,7 +6,6 @@ git config commit.template .devcontainer/git/linkorb_commit.template
 cp .devcontainer/git/hooks/pre-push .git/hooks/pre-push
 chmod +x .git/hooks/pre-push
 
-composer config --global --auth http-basic.repo.packagist.com "$GITHUB_USER" "$PACKAGIST_TOKEN"
 
 composer install
 
