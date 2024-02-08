a22b70c6a3540dd9bfd65e9a36e4d05a6d064e91
Marius Ghita <ghita.v.marius@gmail.com>
2024-02-08 15:07:57 +0200

    chore: repo-ansible run #8358

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 # Managed by https://github.com/linkorb/repo-ansible. Manual changes will be overwritten.
 
+# Workaround for recent Python versions which prevent global pip package installation without an explicit flag
+# or removal of a certain file.
+sudo rm /usr/lib/python3.*/EXTERNALLY-MANAGED || true
+
 git config commit.template .devcontainer/git/linkorb_commit.template
 
 cp .devcontainer/git/hooks/pre-push .git/hooks/pre-push
@@ -8,4 +12,5 @@ chmod +x .git/hooks/pre-push
 
 
 composer install
+
 
