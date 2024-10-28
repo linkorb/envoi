80df48992ba84e11a6570c213ac6b35c3ecbd65f
Marius Ghita <ghita.v.marius@gmail.com>
2024-10-28 17:59:05 +0200

    chore: repo-ansible run

--- .devcontainer/postStart.sh
+++ .devcontainer/postStart.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
 # Managed by https://github.com/linkorb/repo-ansible. Manual changes will be overwritten.
 
+# pre-push composer-unused hook disabled. See Card #8980
+if [ -f .git/hooks/pre-push ]; then
+  diff .devcontainer/git/hooks/pre-push .git/hooks/pre-push
+  if [ $? -eq 0 ]; then
+    rm .git/hooks/pre-push
+  fi
+fi
